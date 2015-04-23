53962aa40dfec6c9812e0586c8ee4b1f3b3a1471
Brandon Sterne <brandon@hackmill.com>
2015-04-22 21:55:33 -0700

    added wrapper class for Koken, added API to create Koken album

--- gallery2koken/automate.py
+++ gallery2koken/automate.py
@@ -3,15 +3,28 @@ import config
 
 def main(args):
     gallery2 = utils.Gallery2(config.GALLERY_2_BASE_URL)
+    koken = utils.Koken(config.KOKEN_BASE_URL)
+
+    if args.http_debug:
+        utils.setup_http_debug()
+
     if args.command == "login":
         gallery2.login()
+
     if args.command == "fetch-albums":
         utils.pretty_print(gallery2.fetch_albums())
+
     if args.command == "fetch-album-images":
         utils.pretty_print(gallery2.fetch_album_images(args.album_name))
 
     if args.command == "fetch-album-image-files":
         gallery2.fetch_album_image_files(args.album_name)
+
+    if args.command == "migrate-albums":
+        gallery2.migrate_albums()
+
+    if args.create_koken_album:
+        koken.create_album(args.album_name)
 
 if __name__ == "__main__":
     config.ARGS = utils.parse_args()
--- gallery2koken/utils.py
+++ gallery2koken/utils.py
@@ -1,28 +1,45 @@
 import argparse
 import requests
+import logging
 import base64
 import urlparse
 import config
 import json
 import re
+import httplib
 
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("--command",
                         choices=["login", "fetch-albums", "fetch-album-images",
-                                 "fetch-album-image-files"],
+                                 "fetch-album-image-files", "migrate-albums"],
                         help="Specify command to run")
     parser.add_argument("--album-name", help="Album name to run command on")
+    parser.add_argument("--create-koken-album", action="store_true", default=False,
+                        help="Create new album in Koken")
     parser.add_argument("--gallery-local", action="store_true", default=False,
                         help="Send requests to Gallery 2 server via localhost")
+    parser.add_argument("--koken-local", action="store_true", default=False,
+                        help="Send requests to Koken server via localhost")
+    parser.add_argument("--http-debug", action="store_true", default=False,
+                        help="Extra HTTP debugging output")
     args = parser.parse_args()
-    if (args.command in ["fetch-album-images", "fetch-album-image-files"] and
+    if ((args.command in ["fetch-album-images", "fetch-album-image-files"] or
+         args.create_koken_album) and
         not args.album_name):
         parser.error(
-            "--fetch-album-images and --fetch-album-image-files " +
+            "--fetch-album-images, --fetch-album-image-files, and --create-koken-album " +
             "also require --album-name be specified."
         )
     return args
+
+def setup_http_debug():
+    httplib.HTTPConnection.debuglevel = 1
+    logging.basicConfig()
+    logging.getLogger().setLevel(logging.DEBUG)
+    requests_log = logging.getLogger("requests.packages.urllib3")
+    requests_log.setLevel(logging.DEBUG)
+    requests_log.propagate = True
 
 class Gallery2(object):
     def __init__(self, url):
@@ -113,6 +130,58 @@ class Gallery2(object):
 
     def fetch_image(self, image_id):
         return requests.get("%s%s" % (self.base_url, image_id), headers = self.headers)
+
+    # main wrapper function to iterate through Gallery 2 albums and create corresponding
+    # albums in Koken
+    def migrate_albums(self):
+        if self.auth_token is None:
+            self.login()
+
+        albums = self.fetch_albums()
+        for album_id in [key for key in albums.keys() if key.startswith("album.name")]:
+            album_id_num = re.search("\d+$", album_id).group()
+            # collect all the relevant information about this album before we export it
+            title = albums.get("album.title.%s" % album_id_num)
+            summary = albums.get("album.summary.%s" % album_id_num)
+            name_id = albums.get("album.name.%s" % album_id_num)
+            # get the images for this album
+            images = self.fetch_album_images(name_id)
+            
+            pretty_print(images)
+            print ""
+
+class Koken(object):
+    def __init__(self, url):
+        self.url = url
+        self.session = requests.Session()
+        self.headers = {"X-Koken-Auth": "cookie"}
+        # modify some attributes if we are accessing Koken via localhost
+        if config.ARGS.koken_local:
+            parts = urlparse.urlparse(self.url)
+            self.url = urlparse.urlunparse(parts._replace(netloc="localhost"))
+            # access virtual host by replacing Host header from the original URL
+            self.headers["Host"] = parts.netloc
+
+    def login(self):
+        if self.session.cookies.get("koken_session_ci") is not None:
+            return
+
+        data = {
+            "email": config.KOKEN_USERNAME,
+            "password": config.KOKEN_PASSWORD
+        }
+        url = "%s/api.php?/sessions" % self.url
+        self.session.post(url, data = data, headers = self.headers)
+
+    def create_album(self, album_name):
+        self.login()
+        url = "%s/api.php?/albums" % self.url
+        data = {
+            "title": album_name,
+            "album_type": 0, # normal album
+            "visibility": "public"
+        }
+        self.session.post(url, data = data, headers = self.headers)
 
 def pretty_print(obj):
     print json.dumps(obj, indent=2, separators=(',', ': '))
