e6a4cfbac6f9ca075c47569a601fa6db474a87e4
Brandon Sterne <brandon@hackmill.com>
2015-04-26 09:23:33 -0700

    added Koken configuration to template

--- gallery2koken/config.example.py
+++ gallery2koken/config.example.py
@@ -1,3 +1,7 @@
 GALLERY_2_BASE_URL = "http://www.example.com/g2_path/main.php"
 GALLERY_2_USERNAME = "username"
 GALLERY_2_PASSWORD = "password"
+
+KOKEN_BASE_URL = "http://www.example.com/koken"
+KOKEN_USERNAME = "username"
+KOKEN_PASSWORD = "password"
